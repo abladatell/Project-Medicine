5bd3754e0aae6464f60331ccc0681b69467778c7
Patrick <pkhwong.92@gmail.com>
2018-11-24 20:04:04 -0800

    Redesign on the web application homepage
    Redesign

--- home/jquery.js
+++ home/jquery.js
@@ -2,100 +2,120 @@ $(document).ready(function() {
   $("body, html").css({
     "margin" : "0px",
     "padding" : "0px",
-    "background" : "url(./images/landscape.jpg)",
-    "background-size" : "cover"
+    "background-color" : "#DCDCDC"
   });
 
-  $("#overlay").css({
-    "position" : "absolute",
+  $("#myheader").css({
     "width" : "100%",
-    "height" : "100%",
-    "background" : "rgba(0, 0, 0, 0.5)",
-    "background-size" : "cover"
+    "height" : "100px",
+    "background-color" : 	"#383838",
   });
 
-  $("#mainbox").css({
-    "width" : "1000px",
-    "height" : "600px",
-    "background-color" : "#FFFFFF",
-    "margin-left" : "auto",
-    "margin-right" : "auto",
-    "margin-top" : "100px",
-    "border-radius" : "3%"
-  });
+  $("body").append("<div id='navigation'></div>");
+  $("body").append("<div id='contentbackground'></div>");
 
-  $("#mainbox").append("<div id='leftside'></div>");
-  $("#mainbox").append("<div id='rightside'></div>");
-
-  $("#leftside, #rightside").css({
-    "height" : "100%",
+  $("#navigation").css({
+    "width" : "100%",
+    "height" : "60px",
     "float" : "left",
-    "border-radius" : "3%"
+    "background-color" : "#DCDCDC",
+    "border-bottom" : "1px #696969 solid"
   });
 
-  $("#leftside").css({
-    "width" : "60%"
-  });
-
-  $("#rightside").css({
-    "width" : "40%",
-    "background-color" : "#F5F5F5"
-  });
-
-  $("#leftside").append("<div id='topleft'></div>");
-  $("#leftside").append("<div id='bottomleft'></div>");
-
-  $("#topleft, #bottomleft").css({
+  $("#contentbackground").css({
     "width" : "100%",
+    "height" : "750px",
     "float" : "left"
   });
 
-  $("#topleft").css({
-    "height" : "25%",
-    "text-align" : "center",
-    "margin-top" : "0.5em",
-  });
+  $("#myheader").append("<div class='frame' id='headercontent' class='tempcolor'></div>");
+  $("#navigation").append("<div class='frame' id='navigationcontent'></div>");
+  $("#contentbackground").append("<div class='frame' id='schedule'></div>");
 
-  $("#bottomleft").css({
-    "height" : "75%"
-  });
-
-  $("#rightside").append("<button id='add'>Add Medication</button> <br><br>");
-  $("#rightside").append("<button id='delete'>Delete</button>");
-
-  $("#add, #delete").css({
-    "font-size" : "2.5em",
-    "padding" : "0.5em",
+  $(".frame").css({
+    "width" : "1160px",
+    "height" : "100%",
     "margin-left" : "auto",
     "margin-right" : "auto",
-    "display" : "block"
+    "padding-left" : "20px",
+    "padding-right" : "20px"
   });
 
-  $("#add").css("margin-top", "50%");
+  $(".tempcolor").css("background-color", "green");
 
-  $("#topleft").append("<h1>Today's Medication</h1>");
-
-  $("h1").css({
-    "margin-top" : "50px",
-    "margin-bottom" : "10px"
+  $("#headercontent").css({
+    "padding-top" : "10px",
+    "padding-bottom" : "10px"
   });
 
-  $("#bottomleft").append("<div id='todaylist'></div>");
 
-  $("#todaylist").css({
-    "margin-left" : "auto",
-    "margin-right" : "auto",
+  $("#headercontent").append("<img id='logoimg' src='https://dummyimage.com/60x60/000/fff' alt='logo'>");
+  $("#headercontent").append("<span id='title'>myMedicationApp</span>");
+  $("#headercontent").append("<span class='headerwelcome'>User!</span>");
+  $("#headercontent").append("<span class='headerwelcome'>Welcome </span>");
+
+  $("#logoimg").css({
+    "font-size" : "2em",
     "display" : "block",
-    "background-color" : "#E0E0E0",
-    "width" : "90%",
-    "height" : "90%",
-    "border-radius" : "10%",
-    "border-color" : "grey",
-    "border-style" : "solid",
-    "border-width" : "5px"
+    "margin-top": "10px",
+    "margin-bottom": "20px",
+    "float" : "left"
   });
 
-  $("*").css("font-family" , "Quicksand, sans-serif");
+  $("#title").css({
+    "font-size" : "2em",
+    "color" : "white",
+    "font-family" : "Fredericka the Great, cursive",
+    "display" : "block",
+    "margin-top": "20px",
+    "margin-left" : "10px",
+    "margin-bottom": "20px",
+    "float" : "left"
+  });
+
+  $(".headerwelcome").css({
+    "font-size" : "1.8em",
+    "color" : "white",
+    "font-family" : "Fredericka the Great, cursive",
+    "display" : "block",
+    "margin-top": "20px",
+    "margin-left" : "10px",
+    "margin-bottom": "20px",
+    "float" : "right"
+  });
+
+  $("#navigationcontent").append("<button class='buttons' id='add'>Add Medication</button>");
+  $("#navigationcontent").append("<button class='buttons' id='delete'>Delete</button>");
+  $("#navigationcontent").append("<span id='logout'>Logout</span>");
+
+  $(".buttons").css({
+    "font-family" : "Open Sans, sans-serif",
+    "margin-right" : "10px",
+    "font-size" : "1.5em",
+    "margin-top" : "10px"
+  });
+
+  $("#logout").css({
+    "font-family" : "Open Sans, sans-serif",
+    "font-size" : "1.5em",
+    "float" : "right",
+    "margin-top" : "10px"
+  });
+
+  $("#schedule").append("<div id='date'></div>");
+  $("#schedule").append("<div id='schedule_list'></div>");
+
+  $("#date").css({
+    "width" : "100%",
+    "height" : "6%",
+  })
+
+  $("#schedule_list").css({
+    "width" : "100%",
+    "height" : "94%",
+    "background-color" : "#C8C8C8",
+    "border-top" : "2px #696969 solid "
+  })
 
   var todaydate = new Date();
 
@@ -127,53 +147,22 @@ $(document).ready(function() {
     }
   }
 
-  $("#topleft").append("<div id='leftarrow'></div>");
-  $("#topleft").append("<div id='dateselect'></div>");
-  $("#topleft").append("<div id='rightarrow'></div>");
+  $("#date").append(myMonth() + " ");
+  $("#date").append(todaydate.getDate() + ", ");
+  $("#date").append(todaydate.getFullYear());
 
-  $("#leftarrow").append("<img class='dateicon' id='lefticon' src='./images/left-arrow.svg'>");
-  $("#dateselect").append(myMonth() + " ");
-  $("#dateselect").append(todaydate.getDate() + ", ");
-  $("#dateselect").append(todaydate.getFullYear());
-  $("#rightarrow").append("<img class='dateicon' id='righticon' src='./images/right-arrow.svg'>");
-
-  $("#leftarrow, #dateselect, #rightarrow").css({
-    "float" : "left",
-    "display" : "block"
+  $("#date").css({
+    "font-family" : "Open Sans, sans-serif",
+    "font-size" : "1.5em",
+    "padding-top" : "10px",
+    "text-align" : "center",
+    "font-weight" : "bold"
   });
 
-  $("#dateselect").css({
-    "width" : "40%",
-    "font-size" : "1.5em"
-  });
 
-  $("#leftarrow, #rightarrow").css({
-    "width" : "30%"
-  });
 
-  $(".dateicon").css({
-    "height" : "40px",
-    "width" : "40px"
-  });
 
-  $("#rightside").append("<div id='logoutdiv'><button id='logout'>Log out </button></div>");
 
-  $("#logoutdiv").css({
-    "width" : "100%",
-    "height" : "150px",
-    "font-size" : "1.3em",
-    "float" : "right"
-  });
 
-  $("#logout").css({
-    "position" : "relative",
-    "padding" : "5px",
-    "padding-left" : "10px",
-    "padding-right" : "10px",
-    "font-size" : "1em",
-    "float" : "right",
-    "right" : "30px",
-    "top" : "110px"
-  });
 
 });
