b8007c6fdbe20b4cfc87592dcd696bb8a2a62ca2
Patrick <pkhwong.92@gmail.com>
2018-12-03 16:28:57 -0800

    Small changes
    Small changes

--- home/jquery.js
+++ home/jquery.js
@@ -324,7 +324,7 @@ $(document).ready(function() {
     scheduleStore.push([name, key]);
   }
 
-  //delette is the index number of the schedule.
+  //delete is the index number of the schedule.
   function deleter(deletee){
     var array = scheduleStore[deletee];
     var key = array[1];
